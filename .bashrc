64f2818a47e5fa33f0d3b202386ba0a5d6590669
Jaxxen <jc1kp@proton.me>
2025-05-15 16:24:51 -0500

    conversion from foosh-bash commit #63

---
+++ .bashrc
@@ -0,0 +1,15 @@
+eval "$(starship init bash)"
+[[ $- != *i* ]] && return
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+PS1='[\u@\h \W]\$ '
+alias os='ollama start'
+alias ds='ollama run deepseek-r1:14b'
+alias fc='sudo theclicker run -d"/dev/input/event4" -l276 -r275 -c25 -C0 -H'
+alias sc='sudo theclicker run -d"/dev/input/event4" -l276 -r275 -c1000 -C0'
+alias ue='~/ue/Engine/Binaries/Linux/UnrealEditor'
+alias e='nvim'
+alias se='sudo nvim'
+alias ff='fastfetch'
+alias y='yazi'
+alias sy='sudo yazi'
---
+++ foo.sh
@@ -0,0 +1,109 @@
+#!/bin/zsh
+
+cd ~/foosh-bash
+
+mv -f {.,}* ~
+
+cd
+
+sudo mv pacman.conf /etc
+
+sudo pacman -Syu
+
+git clone https://aur.archlinux.org/paru.git
+
+cd paru
+
+makepkg -si
+
+cd
+
+packages=(
+  discord
+  blender
+  btop
+  eog
+  fastfetch
+  foot
+  galculator
+  gimp
+  gnome-keyring
+  gnome-themes-extra
+  gtk-engine-murrine
+  hyprland
+  hyprlock
+  hyprpaper
+  hyprsunset
+  libreoffice-fresh
+  ly
+  mako
+  mpv
+  nwg-look
+  obs-studio
+  pavucontrol
+  pcmanfm-gtk3
+  pipewire-pulse
+  prismlauncher
+  qbittorrent
+  rnote
+  signal-desktop
+  starship
+  ttf-cascadia-code-nerd
+  ttf-font-awesome
+  ufw
+  waybar
+  wev
+  wine-gecko
+  wine-mono
+  wofi
+  xdg-desktop-portal-hyprland
+  yazi
+  alarm-clock-applet
+  bibata-cursor-theme-bin
+  chromium-snapshot-bin
+  github-desktop-bin
+  gruvbox-dark-icons-gtk
+  hyprshot
+  librewolf-bin
+  theclicker
+)
+
+for package in $packages; do
+  paru -S --noconfirm $package
+done
+
+paru -S steam
+
+paru -S amd-ucode
+
+paru -S nvidia
+
+paru -S nvidia-settings
+
+xdg-settings set default-web-browser librewolf.desktop
+
+cd graphite-gtk-theme
+
+./install.sh --tweaks rimless black
+
+cd
+
+gsettings set org.gnome.desktop.interface gtk-theme 'Graphite-Dark'
+
+gsettings set org.gnome.desktop.interface icon-theme 'gruvbox-dark-icons-gtk'
+
+sudo systemctl enable ly
+
+rm -rf .git
+
+rm -r foosh-bash
+
+rm -r graphite-gtk-theme
+
+rm LICENSE
+
+rm README.md
+
+rm foo.sh
+
+echo finished, reboot your computer
